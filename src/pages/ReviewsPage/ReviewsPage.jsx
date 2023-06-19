a3fb8449e53ffb23cbda9fb1791d22e99da2e5d6
SHATE\Lahodzich <sagodich@mail.ru>
2023-06-19 18:05:27 +0300

    swiper

--- src/pages/ReviewsPage/ReviewsPage.jsx
+++ src/pages/ReviewsPage/ReviewsPage.jsx
@@ -1,43 +1,45 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import CustomPageBlock from "../../components/CustomPageBlock/CustomPageBlock";
 
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Navigation, Pagination, Autoplay } from "swiper";
 
 //Core
 import { reviews } from "./core/reviews";
 //Style
 import "./ReviewsPageStyle.scss";
 import "swiper/css";
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const ReviewsPage = () => {
   return (
     <CustomPageBlock id={"reviews"} title={"Отзывы"}>
       <div className="reviews">
         <Swiper
-          slidesPerView={3} slidesPerColumn={2} spaceBetween={30}
-          loop={true}
+        slidesPerView={1}
+        centeredSlides={true}
+        spaceBetween={-1050}
+        loop={true}
+        className="mySwiper"
         >
-            {reviews.map((el, index) => {
-              return (
-                <SwiperSlide>
-                    <div className="review-block" key={index}>
-                      <div className="review-text">{el.text}</div>
-                      <div className="review-stars">
-                        <img src="/images/Reviews/GoldStars.svg" />
-                        <img src="/images/Reviews/GoldStars.svg" />
-                        <img src="/images/Reviews/GoldStars.svg" />
-                        <img src="/images/Reviews/GoldStars.svg" />
-                        <img src="/images/Reviews/Star.svg" />
-                      </div>
-                      <div className="review-name">{el.name}</div>
-                    </div>
-                </SwiperSlide>
-              );
-            })}
+          {reviews.map((el, index) => {
+            return (
+              <SwiperSlide className="test3">
+                <div className="review-block" key={index}>
+                  <div className="review-text">{el.text}</div>
+                  <div className="review-stars">
+                    <img src="/images/Reviews/GoldStars.svg" alt="img" />
+                    <img src="/images/Reviews/GoldStars.svg" alt="img" />
+                    <img src="/images/Reviews/GoldStars.svg" alt="img" />
+                    <img src="/images/Reviews/GoldStars.svg" alt="img" />
+                    <img src="/images/Reviews/Star.svg" alt="img" />
+                  </div>
+                  <div className="review-name">{el.name}</div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
       </div>
     </CustomPageBlock>
